37202f4a40699fd39ee71816ff0b6b2c10e310f7
Jakub Boukal <www.bagr@gmail.com>
2018-10-11 18:54:49 +0200

    Add test for product detail Fixes #1

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-
+import asyncio
+from aioresponses import aioresponses
 from pygramed import PyGramedia
 
 
@@ -13,6 +14,30 @@ class PyGramediaTest(unittest.TestCase):
 
     def test_repr_category(self):
         self.assertEqual(str(self.pg.category), '<CategoryAPI: https://www.gramedia.com/api/categories/>')
+
+    @aioresponses()
+    def test_product_retrieve_detail(self, mocked=None):
+        expected = {
+            "name": "Perpajakan Bendahara Desa",
+            "authors": [
+                {
+                    "title": "M.bahrun Nawawi",
+                    "href": "https://www.gramedia.com/api/authors/author-mbahrun-nawawi/"
+                }
+            ],
+            "formats": [
+                {
+                    "name": "Perpajakan Bendahara Desa",
+                    "title": "Soft Cover",
+                }
+            ],
+            "thumbnail": "https://cdn.gramedia.com/uploads/items/9789790625433_perpajakan-bendahara-desa.jpg",
+            "href": "https://www.gramedia.com/api/products/perpajakan-bendahara-desa/",
+        }
+        loop = asyncio.get_event_loop()
+        mocked.get('https://www.gramedia.com/api/products/?per_page=1', payload=dict(**expected))
+        resp = loop.run_until_complete(self.pg.product.retrieve(limit=1))
+        self.assertIn('perpajakan-bendahara-desa', resp['href'])
 
 
 if __name__ == '__main__':
